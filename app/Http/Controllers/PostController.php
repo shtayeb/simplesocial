3b71246265530c5d47cc2cc762d185d02ab24ac1
shtayeb <shahriar.safi@gmail.com>
2021-02-13 21:42:06 +0430

    initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth'])->only(['store','destroy']);
+    }
+    
+    
+    public function index()
+    {   
+        $posts = post::latest()->with(['user','likes'])->paginate(20);
+
+        return view('posts.index',[
+            'posts' => $posts
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'body' => 'required'
+        ]);
+        
+        // Post::create([
+        //     'user_id' => auth()->id(),
+        //     'body' => $request->body
+        // ]);
+
+        // auth()->user()->posts()->create();
+
+        $request -> user() -> posts() -> create([
+            'body' => $request->body
+        ]);
+
+        return back();
+    }
+
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete',$post);
+        $post->delete();
+        return back();
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show',[
+            'post' => $post
+        ]);
+    } 
+}
