9c8e5a400552585e6ecdc3ca3e1ed560081b9596
shtayeb <shahriar.safi@gmail.com>
2021-02-14 08:31:37 +0430

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -45,13 +51,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => $url,
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -62,6 +68,26 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+		
+		//'mysql' => [
+          //  'driver' => 'mysql',
+            //'url' => env('DATABASE_URL'),
+            //'host' => env('DB_HOST', '127.0.0.1'),
+    //        'port' => env('DB_PORT', '3306'),
+      ///      'database' => env('DB_DATABASE', 'forge'),
+         //   'username' => env('DB_USERNAME', 'forge'),
+       //     'password' => env('DB_PASSWORD', ''),
+     //       'unix_socket' => env('DB_SOCKET', ''),
+   //         'charset' => 'utf8mb4',
+        //    'collation' => 'utf8mb4_unicode_ci',
+       //     'prefix' => '',
+      //      'prefix_indexes' => true,
+      //      'strict' => true,
+       //     'engine' => null,
+        //    'options' => extension_loaded('pdo_mysql') ? array_filter([
+         ///       PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+           // ]) : [],
+        //],
 
         'pgsql' => [
             'driver' => 'pgsql',
