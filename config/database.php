679ce52050e9fe29e373b7f341092eeb072fcf28
shtayeb <shahriar.safi@gmail.com>
2021-03-15 13:56:32 +0430

    first laravel deployment to heroku

--- config/database.php
+++ config/database.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$url_ = $url["url"];
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 return [
 
     /*
@@ -51,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => $url_,
-            'host' => $host,
-            //'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -68,26 +62,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-		
-		//'mysql' => [
-          //  'driver' => 'mysql',
-            //'url' => env('DATABASE_URL'),
-            //'host' => env('DB_HOST', '127.0.0.1'),
-    //        'port' => env('DB_PORT', '3306'),
-      ///      'database' => env('DB_DATABASE', 'forge'),
-         //   'username' => env('DB_USERNAME', 'forge'),
-       //     'password' => env('DB_PASSWORD', ''),
-     //       'unix_socket' => env('DB_SOCKET', ''),
-   //         'charset' => 'utf8mb4',
-        //    'collation' => 'utf8mb4_unicode_ci',
-       //     'prefix' => '',
-      //      'prefix_indexes' => true,
-      //      'strict' => true,
-       //     'engine' => null,
-        //    'options' => extension_loaded('pdo_mysql') ? array_filter([
-         ///       PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-           // ]) : [],
-        //],
 
         'pgsql' => [
             'driver' => 'pgsql',
